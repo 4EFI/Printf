36c3eaca344f8b11948976989a7bb8b3803c242b
4EFI <kvv204@gmail.com>
2023-03-16 16:53:15 +0300

    First nasm programm

---
+++ hello.s
@@ -0,0 +1,18 @@
+
+section .text
+global _start
+
+_start:         mov rax, 0x01
+                mov rdi, 1
+                mov rsi, Msg
+                mov rdx, MsgLen 
+                syscall
+
+                mov rax, 0x3c
+                xor rdi, rdi
+                syscall
+
+section .data
+
+Msg     db      "Hello World!!!", 0x0A
+MsgLen  equ     $ - Msg 
