894137372b5ee6515f875f19614df5b03af77759
4EFI <kvv204@gmail.com>
2023-03-20 15:53:43 +0300

    More tests

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,11 @@
 
-extern "C" void c_printf( const char* template_string, ... );
+extern "C" void c_printf( const char* str, ... );
 
 int main()
 {    
-    c_printf( "Vlados %s %d %b %x %o %c %d %d %d\n", "hi-hi-ha", 15, 15, 15, 15, 'a', 15, 15, 15 );
+    c_printf( "Vlados %% %s %% %d %b %x %o %c %d %d %d\n", "hi-hi-ha", 15, 15, 15, 15, 'a', 15, 15, 15 );
+
+    c_printf( "Sanya %d %d %d %d %d %d %d\n", 1, 1, 1, 1, 1, 1, 1 );
 
     return 0;
 }
